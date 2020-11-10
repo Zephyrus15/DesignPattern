6e51a9497d23bce6eab08879377c703f4e329351
Your Name <you@example.com>
2020-11-10 11:53:34 +0100

    initial commit

--- MonSingleton.java
+++ MonSingleton.java
@@ -2,35 +2,18 @@ package com.tactfactory.designpatternniveau1.singleton.tp1;
 
 java.util.*;
 
-public class MonSingleton {
-
-	  private MonSingleton() {
-	    task = new FutureTask<Boolean>(new Callable<Boolean>() {
-
-	      @Override
-	      public Boolean call() throws Exception {
-	        System.out.println(Thread.currentThread().getName());
-	        return true;
-	      }
-	    });
-	  }
-
-	  private static MonSingleton INSTANCE = null;
-
-	  public static synchronized MonSingleton getInstance() {
-	    if (INSTANCE == null) {
-	      INSTANCE = new MonSingleton();
-	    }
-	    return INSTANCE;
-	  }
-
-	  private final FutureTask<Boolean> task;
-	  private final ExecutorService executor = Executors.newFixedThreadPool(100);
-
-	  public Boolean execTask() throws InterruptedException, ExecutionException {
-	    executor.execute(this.task);
-	    executor.awaitTermination(1, TimeUnit.SECONDS);
-	    return this.task.get();
-	  }
-
-	}
+public class MonSingleton
+{
+    private MonSingleton()
+    {}
+     
+    private static MonSingleton INSTANCE = null;
+     
+    public static synchronized MonSingleton getInstance()
+    {           
+        if (INSTANCE == null)
+        {   INSTANCE = new MonSingleton(); 
+        }
+        return INSTANCE;
+    }
+}
